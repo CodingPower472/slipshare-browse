ed88df324ef85aa3d4b12a7eb1ed3539cdc92a0a
Luke <tech.master.champ@gmail.com>
2015-09-01 14:32:01 -0600

    Updated

---
+++ js/main.js
@@ -0,0 +1,146 @@
+function slips(FB_SLIPS_URL, $firebaseArray) {
+  var ref = new Firebase(FB_SLIPS_URL);
+  return $firebaseArray(ref);
+}
+
+function MainController($scope, slips) {
+
+  $scope.inputRow = {};
+
+  $scope.slipcategories = [
+    {
+      boatlength: 22,
+      maxdraft: 14,
+      maxlength: 26
+    },
+    {
+      boatlength: 28,
+      maxdraft: 194,
+      maxlength: 132
+    }
+  ];
+
+  $scope.editedslipcategories = $.extend(true, {}, $scope.slipcategories);
+
+  $scope.fields = [
+    {
+      name: "Boat Length",
+      val: "boatlength",
+      changable: false,
+      unit: 'ft'
+    },
+    {
+      name: "Max Draft",
+      val: "maxdraft",
+      unit: 'ft'
+    },
+    {
+      name: "Max Length",
+      val: "maxlength",
+      unit: 'ft'
+    }
+  ];
+
+  $scope.checkedInputsLen = 0;
+
+  $scope.slipcategoriesLen = $scope.slipcategories.length;
+
+  $scope.selectAllChange = function () {
+
+    var selectAll = $scope.selectAll;
+
+    angular.forEach($scope.slipcategories, function (slipcategory) {
+
+      slipcategory.checked = selectAll;
+
+      selectAll === true ? $scope.checkedInputsLen++ : void 0;
+
+    });
+
+  };
+
+  $scope.reset = function () {
+
+    $scope.editedslipcategories = $scope.slipcategories;
+
+  };
+
+  $scope.selectChange = function (slipcategory) {
+
+    if (slipcategory.checked === true) {
+      $scope.checkedInputsLen++;
+    } else {
+      $scope.checkedInputsLen--;
+    }
+
+    console.log($scope.checkedInputsLen, $scope.slipcategoriesLen)
+
+    if ($scope.checkedInputsLen === $scope.slipcategoriesLen) {
+      $scope.selectAll = true;
+    } else {
+      $scope.selectAll = false;
+    }
+
+  };
+
+  angular.forEach($scope.fields, function (field) {
+
+    if (field.changable === undefined) {
+
+      field.changable = true;
+
+    }
+
+  });
+
+  $scope.submit = function () {
+
+    var len = 0;
+
+    angular.forEach($scope.editedslipcategories, function (slipcategory) {
+
+      if (slipcategory.checked) {
+
+        delete slipcategory.checked;
+
+        slips.$add(slipcategory);
+
+      }
+
+    });
+
+  };
+
+  $scope.setField = function (field) {
+
+    $scope.applied = true;
+
+    var fieldVal = field.val;
+    var inputRowVal = $scope.inputRow[fieldVal];
+
+    field.applied = true;
+
+    if (inputRowVal === null) {
+
+      field.applied = false;
+      $scope.applied = false;
+
+    }
+
+    angular.forEach($scope.editedslipcategories, function (slipcategory) {
+
+      slipcategory[fieldVal] = inputRowVal;
+
+    });
+
+  }
+
+}
+
+angular.module('slipshareV2', ['firebase'])
+
+  .controller('MainController', MainController)
+
+  .constant('FB_SLIPS_URL', 'https://slipshareapp.firebaseio.com/slips')
+
+  .factory('slips', slips);
